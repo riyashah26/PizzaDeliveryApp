5f36ce18199d20aa9a0958a62a5af15700164429
riysshah26 <riyavshah.26@gmail.com>
2022-09-14 00:58:33 -0400

    added folders removed by mistake

---
+++ PizzaDeliveryApp/WebApplication2/Controllers/LoginController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using WebApplication2.Models;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.VisualBasic;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("AllowOrigin")]
+
+    public class LoginController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IWebHostEnvironment _env;
+        public readonly LoginContext _context;
+        public LoginController(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            _configuration = configuration;
+            _env = env;
+        }
+        [HttpPost("LoginDetails")]
+        public IActionResult LoginDetails(Login login ) 
+        {
+            //_context.Login.Where(l=>l.emailId)
+            try
+            {
+                if (_context.Logins != null)
+
+                {
+                    _context.Logins.Add(login);
+                    _context.SaveChanges();
+                    return Ok("Added successfully");
+                }
+            }
+            catch (Exception e)
+            {
+                return (IActionResult)e.GetType();
+            }
+    
+
+            return new JsonResult("Added Successfully");
+        }
+
+    }
+}
---
+++ PizzaDeliveryApp/WebApplication2/Models/Login.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+namespace WebApplication2.Models
+{
+    public class Login
+    {
+        [Key]
+        public string emailId { get; set; } = string.Empty;
+        public string password { get; set; } = string.Empty;
+    }
+}
